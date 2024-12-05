3ef0374b54fea2528013f11ffceb91aff0cd6b7b
[yuzukaR] <[rao18511696926@gmail.com]>
2024-12-05 17:38:53 -0500

    import field to query and add state table

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -1,3 +1,6 @@
+import data.manuscripts.field as flds
+
+AUTHOR_REV = 'AUR'
 COPY_EDIT = 'CED'
 IN_REF_REV = 'REV'
 REJECTED = 'REJ'
@@ -5,11 +8,19 @@ SUBMITTED = 'SUB'
 TEST_STATE = SUBMITTED
 
 VALID_STATES = [
+    AUTHOR_REV,
     COPY_EDIT,
     IN_REF_REV,
     REJECTED,
     SUBMITTED,
 ]
+
+
+SAMPLE_MANU = {
+    flds.TITLE: 'Short module import names in Python',
+    flds.AUTHOR: 'Eugene Callahan',
+    flds.REFEREES: [],
+}
 
 
 def get_states() -> list:
@@ -41,6 +52,43 @@ def get_actions() -> list:
 
 def is_valid_action(action: str) -> bool:
     return action in VALID_ACTIONS
+
+
+def sub_assign_ref(manu: dict) -> str:
+    return IN_REF_REV
+
+
+FUNC = 'f'
+
+STATE_TABLE = {
+    SUBMITTED: {
+        ASSIGN_REF: {
+            # These next lines are alternatives that work the same.
+            # FUNC: sub_assign_ref,
+            FUNC: lambda m: IN_REF_REV,
+        },
+        REJECT: {
+            FUNC: lambda m: REJECTED,
+        },
+    },
+    IN_REF_REV: {
+    },
+    COPY_EDIT: {
+        DONE: {
+            FUNC: lambda m: AUTHOR_REV,
+        },
+    },
+    AUTHOR_REV: {
+    },
+    REJECTED: {
+    },
+}
+
+
+def get_valid_actions_by_state(state: str):
+    valid_actions = STATE_TABLE[state].keys()
+    print(f'{valid_actions=}')
+    return valid_actions
 
 
 def handle_action(curr_state, action) -> str:
