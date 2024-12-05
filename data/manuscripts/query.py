54e86339129155107a5174384dcc1d6b61c161f1
[yuzukaR] <[rao18511696926@gmail.com]>
2024-12-05 17:52:48 -0500

    edit handle_action and testcase relevant to it

--- data/manuscripts/query.py
+++ data/manuscripts/query.py
@@ -91,20 +91,18 @@ def get_valid_actions_by_state(state: str):
     return valid_actions
 
 
-def handle_action(curr_state, action) -> str:
-    if not is_valid_state(curr_state):
-        raise ValueError(f'Invalid state: {curr_state}')
-    if not is_valid_action(action):
-        raise ValueError(f'Invalid action: {action}')
-    new_state = curr_state
-    if curr_state == SUBMITTED:
-        if action == ASSIGN_REF:
-            new_state = IN_REF_REV
-        elif action == REJECT:
-            new_state = REJECTED
-    elif curr_state == IN_REF_REV:
-        if action == ACCEPT:
-            new_state = COPY_EDIT
-        elif action == REJECT:
-            new_state = REJECTED
-    return new_state
+def handle_action(curr_state, action, manuscript) -> str:
+    if curr_state not in STATE_TABLE:
+            raise ValueError(f'Bad state: {curr_state}')
+    if action not in STATE_TABLE[curr_state]:
+            raise ValueError(f'{action} not available in {curr_state}')
+    return STATE_TABLE[curr_state][action][FUNC](manuscript)
+
+
+def main():
+    print(handle_action(SUBMITTED, ASSIGN_REF, SAMPLE_MANU))
+    print(handle_action(SUBMITTED, REJECT, SAMPLE_MANU))
+
+
+if __name__ == '__main__':
+    main()
