c74281e75d9e3b267194f06b401c8a00db7c21ce
Heterohabilis <xm2204@nyu.edu>
2024-12-19 14:43:47 -0500

    undo the roll back

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 # This shell script deploys a new version to a server.
 
 PROJ_DIR=GOODNAME
-VENV=Fall2023
+VENV=myvirtualenv
 PA_DOMAIN="znElaine.pythonanywhere.com"
 PA_USER='znElaine'
 echo "Project dir = $PROJ_DIR"
